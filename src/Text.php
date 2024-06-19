855e3e609f0a71d57d2bd59d057226f31807ccc7
Baptiste Langlade <baptouuuu@gmail.com>
2024-06-19 14:53:39 +0200

    add text view

---
+++ src/Text.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\UI;
+
+use Innmind\Filesystem\File\Content\Line;
+use Innmind\Immutable\{
+    Sequence,
+    Str,
+};
+
+/**
+ * @psalm-immutable
+ */
+final class Text implements View
+{
+    private string $text;
+
+    private function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @psalm-pure
+     */
+    public static function of(string $text): self
+    {
+        return new self($text);
+    }
+
+    public function render(): Sequence
+    {
+        return Sequence::of(Line::of(Str::of($this->text)));
+    }
+}
