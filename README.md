313f6094e6d5ecc9f75c74f925a8ac233bbdcd17
Baptiste Langlade <baptouuuu@gmail.com>
2024-06-19 10:31:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ui
+
+[![Build Status](https://github.com/innmind/ui/workflows/CI/badge.svg?branch=main)](https://github.com/innmind/ui/actions?query=workflow%3ACI)
+[![codecov](https://codecov.io/gh/innmind/ui/branch/develop/graph/badge.svg)](https://codecov.io/gh/innmind/ui)
+[![Type Coverage](https://shepherd.dev/github/innmind/ui/coverage.svg)](https://shepherd.dev/github/innmind/ui)
+
+Description
+
+## Installation
+
+```sh
+composer require innmind/ui
+```
+
+## Usage
+
+Todo
---
+++ blackbox.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+require 'vendor/autoload.php';
+
+use Innmind\BlackBox\{
+    Application,
+    Runner\Load,
+    Runner\CodeCoverage,
+};
+
+Application::new($argv)
+    ->when(
+        \get_env('ENABLE_COVERAGE') !== false,
+        static fn(Application $app) => $app 
+            ->scenariiPerProof(1)
+            ->codeCoverage(
+                CodeCoverage::of(
+                    __DIR__.'/src/',
+                    __DIR__.'/proofs/',
+                )
+                    ->dumpTo('coverage.clover')
+                    ->enableWhen(true),
+            ),
+    )
+    ->tryToProve(Load::everythingIn(__DIR__.'/proofs/'))
+    ->exit();
