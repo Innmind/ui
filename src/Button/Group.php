7f1f6c30cfd055b973f7fe1b49703f7cdbdd63f3
Baptiste Langlade <baptouuuu@gmail.com>
2024-06-19 15:30:25 +0200

    add button group view

---
+++ src/Button/Group.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\UI\Button;
+
+use Innmind\UI\{
+    View,
+    Button,
+    Lines,
+    Indent,
+};
+use Innmind\Immutable\Sequence;
+
+/**
+ * @psalm-immutable
+ */
+final class Group implements View
+{
+    /** @var non-empty-list<Button> */
+    private array $buttons;
+
+    /**
+     * @param non-empty-list<Button> $buttons
+     */
+    private function __construct(array $buttons)
+    {
+        $this->buttons = $buttons;
+    }
+
+    /**
+     * @psalm-pure
+     * @no-named-arguments
+     */
+    public static function of(
+        Button $first,
+        Button ...$rest,
+    ): self {
+        return new self([$first, ...$rest]);
+    }
+
+    public function render(): Sequence
+    {
+        return Lines::of(
+            '<div class="button-group">',
+            ...\array_map(
+                Indent::render(...),
+                $this->buttons,
+            ),
+            '</div>',
+        );
+    }
+}
