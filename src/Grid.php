675fd37d15544224d03cb97df0fb1f89b2805ce1
Baptiste Langlade <baptouuuu@gmail.com>
2024-07-27 15:59:20 +0200

    add grid

---
+++ src/Grid.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\UI;
+
+use Innmind\Immutable\Sequence;
+
+/**
+ * @psalm-immutable
+ */
+final class Grid implements View
+{
+    /** @var Sequence<Card> */
+    private Sequence $cards;
+
+    /**
+     * @param Sequence<Card> $cards
+     */
+    private function __construct(Sequence $cards)
+    {
+        $this->cards = $cards;
+    }
+
+    /**
+     * @psalm-pure
+     *
+     * @param Sequence<Card> $cards
+     */
+    public static function of(Sequence $cards): self
+    {
+        return new self($cards);
+    }
+
+    public function render(): Sequence
+    {
+        return Lines::of(
+            '<div class="grid">',
+            ...$this
+                ->cards
+                ->map(Indent::render(...))
+                ->toList(),
+            ...['</div>'],
+        );
+    }
+}
